874ff80e979472b3ab66cce4a23ea5bfec6654a4
Drahomír Dlabaja <drahomir.dlabaja@chyron.com>
2024-12-11 14:30:56 +0100

    Optimize 2024/12/09

--- 2024/09/part1.py
+++ 2024/09/part1.py
@@ -1,23 +1,26 @@
 import sys
+import collections
+
 
 def sum_range(a, n):
 	return n * a + (n * (n - 1) // 2)
 
+
 def main():
 	line = list(map(int, sys.stdin.read().strip()))
 
-	files = list(enumerate(line[0::2]))
-	spaces = line[1::2]
+	files = collections.deque(enumerate(line[0::2]))
+	spaces = list(reversed(line[1::2]))
 
 	result = 0
 	current_length = 0
 	
 	while files:
-		id, length = files.pop(0)
+		id, length = files.popleft()
 		result += id * sum_range(current_length, length)
 		current_length += length
 
-		space = spaces.pop(0)
+		space = spaces.pop()
 
 		while files and space > 0:
 			id, length = files[-1]
@@ -32,6 +35,7 @@ def main():
 			space -= length
 
 	print(result)
+
 
 if __name__ == "__main__":
 	main()
